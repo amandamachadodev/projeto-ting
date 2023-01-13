a197e375d19b277c90a8defbef34705b2e332fe7
mandioquynha <machado.amandag@gmail.com>
2023-01-13 15:30:37 -0300

    feat: :sparkles: implementada função process
    requisito  3 concluido

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file in str(instance._data):
+        return None
+    txt_dic = txt_importer(path_file)
+    data_process = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_dic),
+        "linhas_do_arquivo": txt_dic
+    }
+    instance.enqueue(data_process)
+    return sys.stdout.write(str(data_process))
+
+    
 
 
 def remove(instance):
