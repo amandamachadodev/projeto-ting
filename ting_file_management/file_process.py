62bd20e494a77a85743750487d850b2b0e3ca4ca
mandioquynha <machado.amandag@gmail.com>
2023-01-13 15:44:00 -0300

    fix: :bug: linhas a mais

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -6,15 +6,15 @@ def process(path_file, instance):
     if path_file in str(instance._data):
         return None
     txt_dic = txt_importer(path_file)
+
     data_process = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(txt_dic),
         "linhas_do_arquivo": txt_dic
     }
+
     instance.enqueue(data_process)
     return sys.stdout.write(str(data_process))
-
-    
 
 
 def remove(instance):
