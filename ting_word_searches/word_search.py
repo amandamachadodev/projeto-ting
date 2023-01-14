67b3ae30c6bce0506156880e32f3b81f95964e9d
mandioquynha <machado.amandag@gmail.com>
2023-01-14 00:55:33 -0300

    feat: :sparkles: implementada função exist_word
    requisito 6 concluido

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    exists = list()
+    result = list()
+    for file in instance._data:
+        for index, line in enumerate(file["linhas_do_arquivo"], start=1):
+            if word.upper() in line.upper():
+                exists.append({"linha": index})
 
+        if len(exists) != 0:
+            result.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": exists,
+            })
+        return result
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
